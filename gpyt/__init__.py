ecd995c2f8a5d627ac61b15af1c5923afa53e1ba
Justin Stitt <jstitt007@gmail.com>
2023-05-05 00:34:36 -0700

    update API error message with diagnostics

--- gpyt/__init__.py
+++ gpyt/__init__.py
@@ -52,13 +52,20 @@ For example, if the user is asking about football use this format
 """
 
 SUMMARY_PROMPT = "Summarize the user's input in 6 words or less. Try to capture the big idea. DO NOT USE more than 6 words and DO NOT answer their question. Simply echo back to them your summary."
-API_ERROR_FALLBACK = [
-    {
-        "choices": [
-            {"delta": {"content": "There was an error with OpenAI, try again later."}}
-        ]
-    }
-]
+
+API_ERROR_MESSAGE = """
+❗ There was an error with OpenAI.
+
+# Diagnostics
+
+Make sure that:
+    1) You have a proper OPENAI_API_KEY configured at `$HOME/.env` starting with "sk-"
+    2) You have configured Billing over at https://platform.openai.com/account/billing
+    3) You aren't getting rate limited (9000 tokens per minute)
+
+Try Again Later.
+"""
+API_ERROR_FALLBACK = [{"choices": [{"delta": {"content": API_ERROR_MESSAGE}}]}]
 
 
 INTRO = "Ask me anything. I'll try to assist you!"
