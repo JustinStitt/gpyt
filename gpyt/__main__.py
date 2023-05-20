0c105b05ba185dba915266969750915b92771997
Justin Stitt <jstitt007@gmail.com>
2023-05-20 13:14:24 -0700

    fix gpyt script

--- gpyt/__main__.py
+++ gpyt/__main__.py
@@ -1,7 +1,12 @@
 from gpyt import app
 
-try:
-    app.run()
 
-except KeyboardInterrupt:
-    print("\n🔧 KeyboardInterrupt detected, cleaning up and quitting.")
+def main():
+    try:
+        app.run()
+
+    except KeyboardInterrupt:
+        print("\n🔧 KeyboardInterrupt detected, cleaning up and quitting.")
+
+
+main()
